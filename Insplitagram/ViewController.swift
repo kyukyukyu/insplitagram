a59c98379bccde5ce339149a43022a1bc396a606
Sanggyu Nam <sanggyu.nam@navercorp.com>
2018-08-02 23:07:16 +0900

    Initial Commit

---
+++ Insplitagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Insplitagram
+//
+//  Created by Sanggyu Nam on 2018. 8. 2..
+//  Copyright © 2018년 Sanggyu Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
