3fd56c31766e3a0a68aa743ba0b19e4c7ad57ffc
Sanggyu Nam <sanggyu.nam@navercorp.com>
2018-08-04 02:07:41 +0900

    Drop AVMutableComposition and set timeRange

--- Insplitagram/Insplitagram.swift
+++ Insplitagram/Insplitagram.swift
@@ -12,25 +12,16 @@ import AVFoundation
 func trimOneMinute(inUrl: URL, outUrl: URL) -> Bool {
     let options = [ AVURLAssetPreferPreciseDurationAndTimingKey: true ]
     let asset = AVURLAsset(url: inUrl, options: options)
-    let videoTrack = asset.tracks(withMediaType: AVMediaType.video).first!
-    let audioTrack = asset.tracks(withMediaType: AVMediaType.audio).first!
     
-    let composition = AVMutableComposition()
-    let videoCompTrack = composition.addMutableTrack(withMediaType: AVMediaType.video, preferredTrackID: CMPersistentTrackID())
-    let audioCompTrack = composition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID: CMPersistentTrackID())
-    
-    let sixtySeconds = CMTimeRangeMake(kCMTimeZero, CMTimeMakeWithSeconds(60, 1))
-    try? videoCompTrack?.insertTimeRange(sixtySeconds, of: videoTrack, at: kCMTimeZero)
-    try? audioCompTrack?.insertTimeRange(sixtySeconds, of: audioTrack, at: kCMTimeZero)
-    
-    let exportSession = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetPassthrough)
+    let exportSession = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetPassthrough)
     guard exportSession != nil else {
         return false
     }
     
+    let sixtySeconds = CMTimeRangeMake(kCMTimeZero, CMTimeMakeWithSeconds(60, 1))
     exportSession?.outputURL = outUrl
     exportSession?.outputFileType = AVFileType.mov
-    //exportSession?.timeRange = CMTimeRangeMake(kCMTimeZero, CMTimeMakeWithSeconds(60, 1))
+    exportSession?.timeRange = sixtySeconds
     
     let dispatchGroup = DispatchGroup()
     dispatchGroup.enter()
